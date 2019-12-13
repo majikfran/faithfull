c786a612e0a861f218b94ccd2066399bb00ed63e
Francis Williams <francis@sourcepulp.com>
2019-12-13 13:17:09 +0000

    gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Stake your ADA with FaithPool - A name you can trust</h1>
+  </Layout>
+)
+
+export default IndexPage
